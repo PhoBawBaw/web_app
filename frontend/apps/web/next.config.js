d1e57d19467a80df05961ba99ebd3b55ed409220
minwook09 <minwook09@unist.ac.kr>
2024-10-16 21:03:31 +0900

    Update .gitignore and remove cached files

--- frontend/apps/web/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: true,
-  output: 'standalone',
-  transpilePackages: ['@frontend/types', '@frontend/ui']
-}
-
-module.exports = nextConfig
